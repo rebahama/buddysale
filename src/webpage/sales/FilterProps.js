03256f11ddef35cc291832f143f070b837dada0b
rebahama <wiri@hotmail.se>
2023-01-15 15:39:22 +0000

    add more style to page

--- src/webpage/sales/FilterProps.js
+++ src/webpage/sales/FilterProps.js
@@ -17,29 +17,30 @@ const FilterProps = (props) => {
   return (
     <Container>
       <Row>
-        <Col className={styles.FlexContainer}md={6}>
-        {images?.splice(0, 1).map((image, index) => (
-            <img key={index} src={image.image} alt="user uploads" className={styles.ImgContainer} />
+        <Col className={styles.FlexContainer} md={6}>
+        <div>
+          <span className={styles.DateSize}> {created_at}</span>
+          
+          <p className={styles.DateSize}> {city_name}</p>
+          {images?.splice(0, 1).map((image, index) => (
+            <img
+              key={index}
+              src={image.image}
+              alt="user uploads"
+              className={styles.ImgContainer}
+            />
           ))}
-<p> {title}</p>
-<p> {city_name}</p>-
-<p> {created_at}</p>
-<p> {owner}</p>
-    <div className={styles.InfoContianer}> 
-          
-          
-          
-          <p> {category_name}</p> 
-          <p> {price}</p>
-         
-         
-          
-          <Link to={`/sales/${id}`}>View more</Link>
-          
+        
+          <p> {title}</p>
           </div>
           
+          <div className={styles.InfoContianer}>
+            <p> Category: {category_name}</p>
+            <p> Price: {price}</p>
+
+            <Link to={`/sales/${id}`}>View more</Link>
+          </div>
         </Col>
-        
       </Row>
     </Container>
   );
