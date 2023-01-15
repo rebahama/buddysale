e458e08a0e54db4f847de534b93682b2defd9b8f
rebahama <wiri@hotmail.se>
2023-01-15 14:42:21 +0000

    add page style

--- src/webpage/sales/FilterProps.js
+++ src/webpage/sales/FilterProps.js
@@ -1,4 +1,6 @@
 import React from "react";
+import styles from "../../styles/FilterProps.module.css";
+import { Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const FilterProps = (props) => {
@@ -13,23 +15,25 @@ const FilterProps = (props) => {
     id,
   } = props;
   return (
-    <div>
-      <div>
-        <div>
+    <Container>
+      <Row>
+        <Col className={styles.FlexContainer}md={6}>
+        {images?.splice(0, 1).map((image, index) => (
+            <img key={index} src={image.image} alt="user uploads" className={styles.ImgContainer} />
+          ))}
           <p> ths is filter prop</p>
-          <p> {owner}</p>
           <p> {title}</p>
+          <p> {owner}</p>
+          
           <p> {category_name}</p>
           <p> {price}</p>
           <p> {city_name}</p>
-          {images?.map((image, index) => (
-            <img key={index} src={image.image} alt="user uploads" />
-          ))}
+         
           <p> {created_at}</p>
           <Link to={`/sales/${id}`}>View more</Link>
-        </div>
-      </div>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
