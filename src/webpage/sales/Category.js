e1ca0a5899a227da96316b10f00bce4ef32169c6
rebahama <wiri@hotmail.se>
2023-01-19 03:53:10 +0000

    add categorypage

--- src/webpage/sales/Category.js
+++ src/webpage/sales/Category.js
@@ -1,9 +1,50 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { axiosReq } from "../../api/axiosDefault";
+import FilterProps from "./FilterProps";
 const Category = () => {
-  return (
-    <div>Category</div>
-  )
-}
+  const [category, setCategory] = useState({
+    results: [],
+  });
 
-export default Category
+  const [categoryLinks, setCategoryLinks] = useState({
+    results: [],
+  });
+
+  useEffect(() => {
+    const handleData = async () => {
+      try {
+        const [{ data: category }, { data: categoryLinks }] = await Promise.all(
+          [
+            axiosReq.get("https://buddy-sale.herokuapp.com/posts/?category=1"),
+            axiosReq.get("https://buddy-sale.herokuapp.com/category/"),
+          ]
+        );
+        setCategory(category);
+        console.log(category);
+        setCategoryLinks(categoryLinks);
+      } catch (err) {}
+    };
+    handleData();
+  }, []);
+  return (
+    <div>
+      {categoryLinks.results.map((category) => {
+        return (
+          <div key={category.id}>
+            <p> {category.title}</p>
+          </div>
+        );
+      })}
+
+{category.results.map((category)=>{
+
+return <FilterProps key={category.id} {...category}/>
+
+
+})}
+      
+    </div>
+  );
+};
+
+export default Category;
