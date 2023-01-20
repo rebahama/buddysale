c18f8282df412a4f70c6a2e0d2a68fed265afcf6
rebahama <wiri@hotmail.se>
2023-01-20 03:09:55 +0000

    add my sale personal page

--- src/webpage/sales/MySales.js
+++ src/webpage/sales/MySales.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { axiosReq } from "../../api/axiosDefault";
+import FilterProps from "./FilterProps";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Loader from "../../components/Loader";
 
 const MySales = () => {
-  return (
-    <div>MySales</div>
-  )
-}
+  const [sale, setSale] = useState({
+    results: [],
+  });
+  const [loaded, loadedcomplete] = useState(false);
+  const currentuser = useCurrentUser();
+  const id = currentuser?.profile_id;
 
-export default MySales
+  useEffect(() => {
+    const handleData = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          `https://buddy-sale.herokuapp.com/posts/?&owner__profile=${id}`
+        );
+        setSale(data);
+        loadedcomplete(true);
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    loadedcomplete(false);
+    handleData();
+  }, []);
+
+  return (
+    <div>
+      {loaded ? (
+        <>
+          
+          {sale.results.map((sale) => {
+            return <FilterProps key={sale.id} {...sale} />;
+          })}
+          
+        </>
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
+};
+
+export default MySales;
