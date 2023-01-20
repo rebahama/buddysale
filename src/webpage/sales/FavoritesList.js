b7cc460045b4b6e723fedd2436eed477f7d4a693
rebahama <wiri@hotmail.se>
2023-01-20 16:59:45 +0000

    add saved posted to display

--- src/webpage/sales/FavoritesList.js
+++ src/webpage/sales/FavoritesList.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { axiosReq } from "../../api/axiosDefault";
+import Loader from "../../components/Loader";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import FilterProps from "./FilterProps";
 
 const FavoritesList = () => {
+
+const [favorite, setFavorite] = useState({
+    results: [],
+  });
+  const [loaded, loadedcomplete] = useState(false);
+  const currentuser = useCurrentUser();
+  const id = currentuser?.profile_id;
+
+  useEffect(() => {
+    const handleData = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          `https://buddy-sale.herokuapp.com/posts/?&favorite__owner__profile=${id}`
+          
+        );
+        setFavorite(data);
+        loadedcomplete(true);
+        
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    loadedcomplete(false);
+    handleData();
+  }, []);
+
   return (
-    <div>FavoritesList</div>
-  )
+    <div>
+      {loaded ? (
+        <>
+          {favorite.results.map((sale) => {
+            return <FilterProps key={sale.id} {...sale} />;
+          })}
+        </>
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
+        
+    
+
 }
 
 export default FavoritesList
