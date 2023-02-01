9dfe8eaef335c23b1f023bf936e91662f9470cde
rebahama <wiri@hotmail.se>
2023-02-01 03:21:41 +0000

    add show and hide from the same user

--- src/webpage/sales/ByUser.js
+++ src/webpage/sales/ByUser.js
@@ -3,26 +3,26 @@ import { useParams } from "react-router-dom";
 import styles from "../../styles/SaleProps.module.css";
 import { axiosReq } from "../../api/axiosDefault";
 import FilterProps from "./FilterProps";
+import { Button } from "react-bootstrap";
 
 const ByUser = (props) => {
   const { id } = useParams();
   const { owner } = props;
-  
+
   const [sale, setSale] = useState({
     results: [],
   });
 
   useEffect(() => {
     const handleData = async () => {
-      
       try {
         const { data } = await axiosReq.get(
           `https://buddy-sale.herokuapp.com/posts/?&owner__profile=${owner}`
         );
-        const datauser=data.results[0].owner
+        const datauser = data.results[0].owner;
         setSale(data);
         console.log(datauser);
-        console.log(id)
+        console.log(id);
       } catch (err) {
         console.log(err);
       }
@@ -30,11 +30,34 @@ const ByUser = (props) => {
     handleData();
   }, []);
 
+  let btnShow = document.getElementById("showAll");
+
+  function myFunction() {
+    let getSale = document.getElementById("saleShow");
+
+    getSale.style.display = "block";
+  }
+
+  btnShow?.addEventListener("click", myFunction);
+
+  const categoryShow = () => {
+    const getSale = document.getElementById("saleShow");
+
+    if (getSale.style.display === "block") {
+      getSale.style.display = "none";
+    } else {
+      getSale.style.display = "block";
+    }
+  };
+
   return (
-    <div className={styles.hideUserSales}>
-      {sale.results.map((sale) => {
-        return <FilterProps key={sale.id} {...sale} />;
-      })}
+    <div>
+      <Button onClick={categoryShow}> Show all sales from user</Button>
+      <div id="saleShow" className={styles.hideUserSales}>
+        {sale.results.map((sale) => {
+          return <FilterProps key={sale.id} {...sale} />;
+        })}
+      </div>
     </div>
   );
 };
