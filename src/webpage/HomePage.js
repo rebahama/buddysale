6e7d0c2beb289de2c355edcf7e7a9ce9bf7c0524
rebahama <wiri@hotmail.se>
2023-02-03 02:11:56 +0000

    add buttons on homepage

--- src/webpage/HomePage.js
+++ src/webpage/HomePage.js
@@ -1,18 +1,32 @@
 import React from "react";
-import pic from "../assets/background.jpg"
-import { NavLink } from "react-router-dom";
-
+import styles from "../styles/HomePage.module.css";
+import { Link, NavLink } from "react-router-dom";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 const HomePage = () => {
   return (
-    <div>
-      <NavLink to="createaccount"> create account </NavLink>
-      <div>
-      <img src={pic} alt="sale"/>
+    <Container fluid>
+      <Row md={12}>
+        <Col className={styles.CityImage}>
+          <NavLink to="createaccount"> create account </NavLink>
+          <h1 className={styles.IntroText}>
 
-      </div>
-     
-    </div>
+            Welcome to this page and you can sell alot of stuff here
+          </h1>
+
+          <h2 className={styles.IntroText}> Click here to create account </h2>
+          <Link className={styles.IntroBtn} to="createaccount">
+            Click here
+          </Link>
+        </Col>
+      </Row>
+
+      <Row md={12} className={styles.BlackBackground}>
+        <Col>
+          <NavLink to="createaccount"> create account </NavLink>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
