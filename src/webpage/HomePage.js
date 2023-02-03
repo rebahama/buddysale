4d6b210afa33d6a985955484901fbc9eef3508c1
rebahama <wiri@hotmail.se>
2023-02-03 01:25:39 +0000

    add homepage pic

--- src/webpage/HomePage.js
+++ src/webpage/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import pic from "../assets/background.jpg"
 import { NavLink } from "react-router-dom";
 
 
@@ -6,6 +7,11 @@ const HomePage = () => {
   return (
     <div>
       <NavLink to="createaccount"> create account </NavLink>
+      <div>
+      <img src={pic} alt="sale"/>
+
+      </div>
+     
     </div>
   );
 };
