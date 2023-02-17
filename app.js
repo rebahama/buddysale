a95576a96a9fd0cef8344a1472b38f7ebba0cf33
rebahama <wiri@hotmail.se>
2023-02-17 21:53:25 +0000

    add server code

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
-
 const path = require('path');
-
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static('build'));
-  app.get('*', (req, res) => {
-    req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
-}
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, (err) => {
   if (err) return console.log(err);
