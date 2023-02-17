dcb783d261b5b81e78cbc9a62748c58bdaf03bf6
rebahama <wiri@hotmail.se>
2023-02-17 02:09:48 +0000

    remove ok

--- src/webpage/sales/SaleProps.js
+++ src/webpage/sales/SaleProps.js
@@ -195,7 +195,7 @@ const SaleProps = (props) => {
                 </Modal.Header>
                 <Modal.Body> {message}</Modal.Body>
                 <Modal.Footer>
-                  <Button> Ok</Button>
+                 
                 </Modal.Footer>
               </Modal>
             </div>
